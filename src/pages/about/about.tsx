e14d47e3d11d328633623ca201b8c92be28410b0
tangjin <tangj@bokecc.com>
2019-10-21 17:41:33 +0800

    修改about

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-export default class Login extends Component {
+export default class About extends Component {
     render() {
         return (
             <div>
