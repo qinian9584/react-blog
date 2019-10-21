64a9cc313e5d2806e362880d9e16af8e877413cc
tangjin <tangj@bokecc.com>
2019-10-21 17:39:20 +0800

    修改about文件

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,9 +1,19 @@
 import React,{Component} from 'react';
-export default class Login extends Component {
-    render() {
+export default class About extends Component {
+    constructor(props) {
+        super(props);
+        // 这边绑定是必要的，这样 `this` 才能在回调函数中使用
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick() {
+       console.log(3456789)
+    }
+ 
+    render() { 
         return (
-            <div>
-                about
+            <div onClick={this.handleClick}>
+                about 345678
             </div>
         )
     }
