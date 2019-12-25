36dc685cb8a352a702e215d0dbf0bb391683e4d3
tangjin <569345623@qq.com>
2019-12-25 15:37:41 +0800

    增加分号

--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Route,Switch,Redirect} from 'react-router-dom'
+import {Route,Switch,Redirect} from 'react-router-dom';
 import { Layout } from 'antd';
 import Header from '../../components/header';
 import LeftNav from '../../components/left-nav';
