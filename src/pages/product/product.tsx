d53335a112b4562b461bf91d1f484748168989e4
tangjin <569345623@qq.com>
2019-12-26 16:24:34 +0800

    增加redux

--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -4,6 +4,7 @@ export interface IAppProps {
 }
 
 export interface IAppState {
+    count: number
 }
 
 export default class Product extends React.Component<IAppProps, IAppState> {
@@ -11,13 +12,30 @@ export default class Product extends React.Component<IAppProps, IAppState> {
         super(props);
 
         this.state = {
+            count: 0
         }
     }
 
+    setCount = ()=>{
+        Promise.resolve().then(val=>{
+            this.setState({count:this.state.count+1},(state?:any)=>{
+                console.log('state', state)
+            })
+            console.log('setState', this.state.count)
+            this.setState({count:this.state.count+1},(state?:any)=>{
+                console.log('state1', state)
+            })
+            console.log('setState1', this.state.count)
+        }) 
+    }
+
     render() {
+        console.log('render', this.state.count)
         return (
             <div className="product">
-                product
+                product 商品管理
+
+                <button onClick={this.setCount}>测试</button>
             </div>
         );
     }
